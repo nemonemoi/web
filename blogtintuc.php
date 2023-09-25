8a97a9b5c4e69e50fc1968f52adcce3d3b980d75
nemonemoi <145965729+nemonemoi@users.noreply.github.com>
2023-09-25 14:19:48 +0700

    Add files via upload

---
+++ blogtintuc.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Blog cá cảnh</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Karma">
+<style>
+body {
+background-color: #000000
+}
+</style>
+<style>
+body,h1,h2,h3,h4,h5,h6 {font-family: Arial", sans-serif}
+.w3-bar-block .w3-bar-item {padding:20px}
+</style>
+</head>
+<body>
+<!-- Sidebar (hidden by default) -->
+<nav class="w3-sidebar w3-bar-block w3-card w3-top w3-xlarge w3-animate-left" style="display:none;z-index:2;width:40%;min-width:300px" id="mySidebar">
+  <a href="javascript:void(0)" onclick="w3_close()"
+  class="w3-bar-item w3-button">Close Menu</a>
+  <a href="#fish" onclick="w3_close()" class="w3-bar-item w3-button">Fish</a>
+  <a href="#about" onclick="w3_close()" class="w3-bar-item w3-button">About</a>
+</nav>
+
+<!-- Top menu -->
+<div class="w3-top">
+  <div class="w3-white w3-xlarge" style="max-width:1200px;margin:auto"> 
+  <!-- <img src="https://www.frostscience.org/wp-content/uploads/2020/05/Untitled-design-22.jpg" alt="Huong-dan-nguoi-moi" style="max-width:1200px;margin:auto" /> -->
+    <div class="w3-button w3-padding-16 w3-left" onclick="w3_open()">☰</div>
+    <div class="w3-right w3-padding-16">Mail</div>
+    <div class="w3-center w3-padding-16">Blog Tin Tức</div>
+  </div>
+</div>
+  
+<!-- !PAGE CONTENT! -->
+<div class="w3-main w3-content w3-padding" style="max-width:1200px;margin-top:100px">
+
+  <!-- First Photo Grid-->
+  <div class="w3-row-padding w3-padding-16 w3-center" id="fish">
+    <div class="w3-quarter">
+	<img src="https://wikicacanh.com/wp-content/uploads/2022/07/lai-tao-ca-vang.jpg" alt="Cá vàng" width="230" height="150">
+	<font color="#FFFFFF">
+      <h3>Phương pháp lai tạo cá vàng</h3>
+      <p>Cá vàng là loài cá nước ngọt nhỏ thường được nuôi làm cảnh. Cá vàng là một trong những dòng cá được thuần hóa...</p>
+	  <p>Xem thêm</p>
+	  </font>
+    </div>
+    <div class="w3-quarter">
+	  <img src="https://image-us.eva.vn/upload/2-2023/images/2023-06-09/1686283480-90-thumbnail-width640height480.jpg" alt="Cá kiểng" width="230" height="150">
+	  <font color="#FFFFFF">
+      <h3>12 loại cá cảnh đẹp cho người mới nuôi</h3>
+      <p>Nỗi băn khoăn lớn nhất của người mới tập nuôi cá cảnh, đó là nên nuôi loại cá nào để dễ chăm sóc, đồng thời đảm bảo khỏe mạnh và ít bị mắc bệnh..</p> 
+	  <p>Xem thêm</p>
+	  </font>
+    </div>
+    <div class="w3-quarter">
+      <img src="https://wikicacanh.com/wp-content/uploads/2022/08/ca-ong-tien.jpg.webp" alt="Cá ông tiên" width="230" height="150">
+	  <font color="#FFFFFF">
+      <h3>10 loại cá cảnh không cần oxy vẫn sống khỏe</h3>
+      <p>Bạn làm một người đam mê nuôi cá cảnh. Bạn chuẩn bị 1 hồ cá cảnh thật đẹp để đặt trong phòng làm việc. Hoặc đặt trong phòng khách...</p>
+	  <p>Xem thêm</p>
+	  </font>
+    </div>
+    <div class="w3-quarter">
+      <img src="https://tepbac.com/upload/news/ge_image/2021/02/nuoi-ca-canh_1614155419.jpg" alt="Cá cảnh" width="230" height="150">
+	  <font color="#FFFFFF">
+      <h3>Thu nhập hàng trăm triệu từ nuôi cá cảnh</h3>
+      <p>Trong khi những bạn trẻ cùng trang lứa còn đang loay hoay tìm cho mình một ngành nghề, một công việc ổn định để lập nghiệp thì anh Nguyễn Ngọc Thạch...</p>
+	  <p>Xem thêm</p>
+	  </font>
+    </div>
+  </div>
+  
+  <!-- Second Photo Grid-->
+  <div class="w3-row-padding w3-padding-16 w3-center">
+    <div class="w3-quarter">
+      <img src="https://toplist.vn/images/200px/loai-ca-canh-nuoc-ngot-de-nuoi-phu-hop-voi-nguoi-moi-bat-dau-194658.jpg" alt="Cá cảnh nước ngọt" width="230" height="150">
+	  <font color="#FFFFFF">
+      <h3>Top 5 Địa chỉ bán cá cảnh uy tín</h3>
+      <p>Cá cảnh đẹp không chỉ là ở sức hút của chúng mà còn ở việc chúng mang đến cho chúng ta những phút giây thư giãn và tạo một không gian đẹp cho ngôi nhà của bạn...</p>
+	  <p>Xem thêm</p>
+	  </font>
+    </div>
+    <div class="w3-quarter">
+      <img src="https://image.vtc.vn/files/f2/2015/12/07/nhung-loai-ca-canh-dat-gia-khien-dai-gia-viet-me-man-0.jpg" alt="Cá huyết rồng" width="230" height="150">
+	  <font color="#FFFFFF">
+      <h3>Những loài cá cảnh đắt giá</h3>
+      <p>Cá huyết rồng, cá rồng bạch tạng, cá chọi Betta, cá 7 màu... là những giống cá cảnh đắt tiền khiến nhiều đại gia Việt mê mẩn và lùng mua.</p>
+	  <p>Xem thêm</p>
+	  </font>
+    </div>
+    <div class="w3-quarter">
+      <img src="https://media-cache-ak0.pinimg.com/736x/34/d0/8b/34d08b6d8e9dd0a5847607f068ceddb0.jpg" alt="Cá được bơm màu" width="230" height="150">
+	  <font color="#FFFFFF">
+      <h3>Những loại cá cảnh được bơm màu</h3>
+      <p>Cá cảnh bơm màu giúp hồ cá của bạn vào ban đêm như những ánh đèn màu lượn khắp hồ , chẳng thua xa gì cá neon, những loại cá này được bơm màu do công nghệ GloFish, lưu ý...</p>
+	  <p>Xem thêm</p>
+	  </font>
+    </div>
+    <div class="w3-quarter">
+      <img src="https://storage.googleapis.com/digital-platform/hinh_anh_top_15_be_ca_canh_dep_lung_linh_cho_dan_dam_me_thuy_sinh_so_1_2bf3d92eb2/hinh_anh_top_15_be_ca_canh_dep_lung_linh_cho_dan_dam_me_thuy_sinh_so_1_2bf3d92eb2.jpg" alt="Bể cá" width="230" height="150">
+	  <font color="#FFFFFF">
+      <h3>Top 15 bể cá cảnh đẹp lung linh</h3>
+      <p>Hiện nay nhu cầu sử dụng bể cá cảnh để làm đẹp cho không gian nhà ở, văn phòng,... ngày càng phổ biến. Để đáp ứng thị hiếu của khách hàng, các mẫu bể cá cảnh cũng được phát...</p>
+	  <p>Xem thêm</p>
+	  </font>
+    </div>
+  </div>
+
+  <!-- Pagination -->
+  <div class="w3-center w3-padding-32">
+    <div class="w3-bar">
+	<font color="#000000">
+      <a href="#" class="w3-bar-item w3-button w3-white">«</a>
+      <a href="#" class="w3-bar-item w3-white w3-button">1</a>
+      <a href="#" class="w3-bar-item w3-button w3-white">2</a>
+      <a href="#" class="w3-bar-item w3-button w3-white">3</a>
+      <a href="#" class="w3-bar-item w3-button w3-white">4</a>
+      <a href="#" class="w3-bar-item w3-button w3-white">»</a>
+	 </font>
+    </div>
+  </div>
+  
+  
+
+</div>
+
+<script>
+// Script to open and close sidebar
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+ 
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
+</script>
+
+</body>
+</html>
